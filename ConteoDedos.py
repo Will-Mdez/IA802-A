6d626468820fdee7c8279c07a2f9b0ebfb6ea9fb
Will-Mdez <82000613+Will-Mdez@users.noreply.github.com>
2022-06-19 21:09:13 -0500

    ConteoDedos

--- ConteoDedos.py
+++ ConteoDedos.py
@@ -1,35 +1,33 @@
 import cv2
-import ContadorManos as sm
+import seguimientoManos as sm
 
-detector=sm.detectormanos(Confdeteccion=0.75)
+detector = sm.detectorManos(Confdeteccion=0.75)
 
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
+
 
 while True:
-    while True:
-        # lectura de la camara
-        ret, frame = cap.read()
-        if ret is False:
-            break
+    ret, frame = cap.read()
 
-        frame = detector.encontrarmanos(frame)
+    frame = detector.encontrarManos(frame)
 
-        cv2.rectangle(frame,(420,225),(570,425),(0,0,0),cv2.FILLED)
+    cv2.rectangle(frame,(420, 225), (570,425), (0,0,0), cv2.FILLED)
+    cv2.putText(frame, "Dedos", (425,420), cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),5)
 
-        cv2.putText(frame,"# Dedos",(425,420),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),5)
+    manosInfo, cuadro = detector.encontrarPosicion(frame, dibujar=False)
 
-        manosInfo,cuadro= detector.encontrarposicion(frame,dibujar=True)
+    if len(manosInfo) != 0:
+        dedos = detector.dedosArriba()
+        contar = dedos.count(1)
+        if contar >= 0 and contar <= 5:
+            cv2.putText(frame, str(contar), (445,375), cv2.FONT_HERSHEY_PLAIN, 10, (0,255,0), 25)
 
-        if len(manosInfo)!=0:
-            dedos=detector.dedoarriba()
-            contar=dedos.count(1)
-            cv2.putText(frame, str(contar), (445, 375), cv2.FONT_HERSHEY_PLAIN, 10, (0, 255, 0), 25)
+    cv2.imshow("Contando dedos", frame)
+    t = cv2.waitKey(1)
+    if t == 27:
+        break
 
-        cv2.inshow('Manos', frame)
-
-        t = cv2.waitKey(1)
-        if t == 27:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
+   
